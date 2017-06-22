1a9759b0b11d4d219c41e8e01127f88902841460
Euan Blackledge <euanblackledge@Euans-MacBook-Pro.local>
2017-06-22 22:34:42 +0100

    starting to sort out the ticks along the x axis

--- client/src/views/GraphView.js
+++ client/src/views/GraphView.js
@@ -5,7 +5,7 @@ function GraphView( debit, credit, transactions ) {
   this.credit = credit;
   this.transactions = transactions;
   this.time = [];
-  this.total = [];
+  this.total = [0];
   this.makePlotPoints();
 }
 
@@ -46,9 +46,15 @@ GraphView.prototype = {
             type: 'datetime',
             categories: [ 'Time' ],
             dateTimeLabelFormats: {
-                        month: '%e. %b',
-                        year: '%b'
-                    },
+                second: '%H:%M:%S',
+                minute: '%H:%M',
+                hour: '%H:%M',
+                day: '%e. %b',
+                week: '%e. %b',
+                month: '%b \'%y',
+                year: '%Y'
+            },
+            tickInterval: 3600000 * 24 * 7
         },
         yAxis: {
 
@@ -61,15 +67,27 @@ GraphView.prototype = {
             }
         },
         series: [{
-            data:  [ this.time ] 
+            data: (function () {
+                console.log( this.time )
+                var data = []
+                for ( var i = 0; i < this.time.length; i++ ) {
+                    data.push([
+                        this.time[i], this.total[i]
+                    ]);
+                }
+                return data;
+            }.bind( this )()),
+            pointInterval: 1000 * 60 * 60
         }]
+
+
     });
   },
 
   makePlotPoints: function() {
     for( var i = 0; i < this.transactions.length; i++ ) {
         this.time.push( this.makeUTC( this.transactions[i].created_at ));
-        this.total.push( this.makeTotal( this.transactions[i] ));
+        this.total.push( this.makeTotal( this.transactions[i], i ));
     }
     console.log( this.time );
     this.display();
@@ -91,11 +109,17 @@ GraphView.prototype = {
     var second = weirdTimeStamp.substr( 17, 2 );
     second = parseInt( second );
 
-    return Date.UTC( year, month, day, hour, minute, second );
+    return Date.UTC( year, month, day );
   },
 
-  makeTotal: function( transaction ) {
-    if( transaction.debit )
+  makeTotal: function( transaction, index ) {
+    if( transaction.debit ) {
+        this.total[ index ] -= transaction.amount
+    } else {
+        this.total[ index ] += transaction.amount
+    }
+    console.log( this.total[ index ]);
+    return this.total[ index ];
   }
 }
 
