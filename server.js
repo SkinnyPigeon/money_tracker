e63a9d3359536bea2e6cd4c831f3769c469eb0fc
Euan Blackledge <euanblackledge@Euans-MacBook-Pro.local>
2017-06-22 23:15:50 +0100

    also forgot about the server, should be sorted now

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.get('/', function (req, res) {
 
 app.use(express.static('client/build'));
 
-var server = app.listen(3000, function () {
+var server = app.listen( process.env.PORT || 3000, function() {
   var host = server.address().address;
   var port = server.address().port;
 
